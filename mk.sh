63d35471b01cb9168716bd4e42b1ff7bf5908b3b
shibacow <shibacow@gmail.com>
2015-11-23 17:35:24 +0000

    ch data

--- mk.sh
+++ mk.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-export ELIXIR_ERL_OPTIONS="+P 100000"
-#iex --sname bar@localhost --cookie foo -S mix run -e "ManyProcessFactory.main/1"  pname=1
+
 mix escript.build
-./many_process --pnum=100000
+export ELIXIR_ERL_OPTIONS="+P 50000000" 
+#iex --sname bar@localhost --cookie foo -S mix run -e "ManyProcessFactory.main" 
+
+#./many_process --pnum=10000
+iex --sname bar@localhost --cookie foo -S mix 
